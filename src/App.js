ed2ebf4c5f2fec6ff548bdc32e4a6cdf46a02a66
fredericbertrand <fredericbertrand888@gmail.com>
2020-10-27 18:12:01 +0100

    publication for quest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Contact from './components/Contact'
 import ContactList from './components/ContactList';
 
 function App() {
