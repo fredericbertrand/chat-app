534061e5f4198a5f42c6434fdb5ea63d4da7688b
fredericbertrand <fredericbertrand888@gmail.com>
2020-10-27 19:46:59 +0100

    add state to change status when click on

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './Contact.css';
 
 function Contact(props) {
+    const [connexion, setConnexion] = useState(props.online)
+
     return (
         <div className="Contact">
             <img src={props.avatar} className="avatar" alt={props.name} />
             <div >
                 <h2 className="name">{props.name}</h2>
                 <div className="status">
-                    <span className={props.online ? "status-online" : "status-offline"}></span>
-                    <span className="status-text">{props.online ? "online" : "offline"}</span>
+                    <button>
+                        <span onClick={() => { setConnexion(!connexion) }} className={connexion ? "status-online" : "status-offline"}></span>
+                        <span onClick={() => { setConnexion(!connexion) }} className="status-text">{connexion ? "online" : "offline"}</span>
+                    </button>
                 </div>
             </div>
         </div>
-
-
     );
 }
 
